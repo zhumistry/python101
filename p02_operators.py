59f7ecfa36b5a9b79eeea09a8268ef4f25c47bda
Gi Soong Chee <gi.soongchee@dhs.sg>
2022-10-12 14:10:02 +0800

    Update p02_operators.py

--- p02_operators.py
+++ p02_operators.py
@@ -1,1 +1,21 @@
 # Operators
+
+# Examples that give the meaning of life
+
+# addition
+print(19 + 23)
+
+# subtraction
+print(50 - 8)
+
+# multiplication
+print(21 * 2)
+
+# division - integer
+print(84 // 2)
+  
+# division - true
+print(84 / 2)
+
+# exponent / power
+print(3**3 + 2**4 - 1) # anyone has a better one?
